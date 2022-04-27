eae138df586c4e4710b0a6171eca612b2c7ed27c
AlbertoT8 <ap_tl8@hotmail.com>
2022-04-27 16:57:37 -0500

    vistas index y rigs

--- db/mongo.js
+++ db/mongo.js
@@ -1,12 +1,12 @@
 const { MongoClient } = require('mongodb');
-const mongoose = require('mongoose')
+//const mongoose = require('mongoose')
 // or as an es module:
 // import { MongoClient } from 'mongodb'
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
-mongoose.connect(url)
+//mongoose.connect(url)
 
 // Database Name
 const dbName = 'botdb';
