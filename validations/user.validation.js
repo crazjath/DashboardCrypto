f5f217e8ea8a26843c29e63d276bff7d01405240
AlbertoT8 <ap_tl8@hotmail.com>
2022-04-27 17:30:48 -0500

    estilo otra vez

--- validations/user.validation.js
+++ validations/user.validation.js
@@ -4,7 +4,7 @@ const authSchema = Joi.object({
     nombre: Joi.string().required(),
     apellido: Joi.string(),
     email: Joi.string().email().lowercase().required(),
-    password: Joi.string().min(4).required()
+    password: Joi.string().required()
   })
   
   module.exports = {authSchema}
