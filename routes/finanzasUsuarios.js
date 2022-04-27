83963f42a54ea18e9dbfb64a5e28ae80057b5a54
rafasq <50592977+rafasq@users.noreply.github.com>
2022-04-27 16:31:02 -0500

    Proteger y Reparar Rutas
    Proteccion de rutas
    Reparar vista de admin inicio

--- routes/finanzasUsuarios.js
+++ routes/finanzasUsuarios.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var {client, dbName} = require("../db/mongo");
+var {ObjectId} = require('mongodb')
+
+passport.deserializeUser(
+    async function(id, done) {
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection("Usuarios");
+    collection.findOne({_id:ObjectId(id)}, function (err, user) {
+      done(err, user);
+    });
+  });
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/',function(req, res, next){
+    if (req.isAuthenticated()) {
+      return next();
+  } else {
+      res.redirect('/')
+  }
+  }, function(req, res, next) {
     res.render('finanzasUsuarios', { title: 'Express' });
-});
+  });
 
 module.exports = router;
--- routes/signupAdmin.js
+++ routes/signupAdmin.js
@@ -1,16 +1,40 @@
 var express = require('express');
 const createHttpError = require('http-errors');
 var router = express.Router();
+var {ObjectId} = require('mongodb')
 var { client, dbName } = require('../db/mongo');
+var passport = require('passport');
 
 const User = require('../models/user.model');
 const { authSchema } = require('../validations/user.validation');
 const bcrypt = require('bcryptjs');
 
+passport.deserializeUser(
+  async function(id, done) {
+  await client.connect();
+  const db = client.db(dbName);
+  const collection = db.collection("Usuarios");
+  collection.findOne({_id: ObjectId(id)}, function (err, user) {
+    console.log(user)
+    done(err, user);
+  });
+});
+
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.render('signupAdmin');
+router.get('/',function(req, res, next){
+  if (req.isAuthenticated()) {
+    return next();
+} else {
+    res.redirect('/')
+}
+}, function(req, res, next) {
+  if(req.user.Profile==="admin"){
+    res.render('signupAdmin', { title: 'Express' });
+  }
+  else{
+    res.redirect('/usuario/inicio')
+  }
 });
 
 router.post("/registro", async (req, res, next) => {
